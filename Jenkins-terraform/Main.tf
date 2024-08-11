cb6a7a6a518acd24b85e2cc3df608f1031aad5fd
onengine-haresh <haresh.nanda@onengine.ai>
2024-08-11 06:38:27 +0530

    added new config

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -59,9 +59,9 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0df4b2961410d4cff"
+  ami                    = "ami-0c2af51e265bd5e0e"
   instance_type          = "t2.medium"
-  key_name               = "purplehaze"
+  key_name               = "new-haresh-kp"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
